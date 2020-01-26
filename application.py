357c6e84c402ea410d1bbca10a627cf4fa9722e1
tomdevalk1997 <tomdevalk@hotmail.nl>
2020-01-26 19:46:50 +0100

    Styling ongeveer af

--- application.py
+++ application.py
@@ -8,13 +8,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask import request
 import requests
-import json
-import datetime
 #from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 #socketio = SocketIO(app)
+
+# messages = {"yes": 0, "no": 0, "maybe": 0}
 
 @app.route("/")
 def index():
@@ -22,5 +22,11 @@ def index():
     status = "Active"
     members = ["Tom", "Rebecca"]
     channel_title = "Channel 1"
-    channels = ["Channel 1", "Channel 2", "Channel 3", "Channel 4", "Channel 5", "Channel 6", "Channel 7", "Channel 8"]
+    channels = ["Channel 1", "Channel 2", "Channel 3", "Channel 4", "Channel 5", "Channel 6", "Channel 7", "Channel 8", "Channel 9", "Channel 10", "Channel 11", "Channel 12", "Channel 13", "Channel 14", "Channel 15", "Channel 16"]
     return render_template("index.html", display_name=display_name, status=status, channel_title=channel_title, channels=channels, members=members)
+
+# @socketio.on("send message")
+# def send_message(data):
+#     selection = data["selection"]
+#     votes[selection] += 1
+#     emit("vote totals", votes, broadcast=True)
